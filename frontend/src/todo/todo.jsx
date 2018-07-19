df80b3582395f8e27731a8bcc0b4161b33151aff
Riallis <riallis.silva@blinks.com.br>
2018-07-19 14:37:03 -0300

    Evento onChange Corrigido

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,24 +1,47 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 import PageHeader from '../tamplate/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
 
+const URL = 'http://localhost:3003/api/todos'
+
 export class Todo extends Component {
   constructor(props){
     super(props)
+    this.state = { description: '', list: []}
+
+    this.handleChange = this.handleChange.bind(this)
     this.handleAdd = this.handleAdd.bind(this)
   }
 
+  handleChange(e){
+    
+    this.setState({...this.state, description: e.target.value})
+
+  }
+
+  refresh(){
+    axios.get(`${URL}?sort=-createdAt`)
+      .then(resp => this.setState({...this.state, description: '', list: resp.data }))
+  }
   handleAdd(){
-    console.log(this)
+    const description = this.state.description;
+    axios.post(URL, {description})
+      .then(resp => refresh())
+
+    console.log('Funcionou!')
+    this.setState({...this.state, description: ''})
   }
  
   render() {
     return (
       <div>
         <PageHeader name="Tarefas" small="Cadastro"></PageHeader>
-        <TodoForm handleAdd={this.handleAdd}/>
+        <TodoForm description={this.state.description}
+        handleChange={this.handleChange}
+        handleAdd={this.handleAdd}/>
         <TodoList/>
       </div>  
     )
